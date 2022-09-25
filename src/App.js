26f423c573b05cc729b049c11bb408a15b52db3b
samiul-sheikh <sheikh.samiul1971@gmail.com>
2022-09-25 23:44:25 +0600

    display post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Counter from "./features/counter/Counter";
+import PostLists from "./features/posts/postsLists"
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <PostLists />
     </div>
   );
 }
--- src/features/posts/postsLists.js
+++ src/features/posts/postsLists.js
@@ -1,7 +1,23 @@
 import React from "react";
+// import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 const postsLists = () => {
-  return <div>postLists</div>;
+  const posts = useSelector((state) => state.posts);
+
+  const renderPosts = posts.map((post) => (
+    <articlec key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </articlec>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderPosts}
+    </section>
+  );
 };
 
 export default postsLists;
